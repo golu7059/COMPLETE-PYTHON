042c39da1f4646f7be9627b66b5e6ab85a5d6fde
golu7059 <golukumarjaisidih@gmail.com>
2024-02-18 21:24:20 +0530

    tuples added

--- class/tuple/marks.py
+++
@@ -1,5 +0,0 @@
-# main difference between list and touples is that list is mutable("We can do cahnges") , while tuple is immutable ("We can't do changes") 
-
-marks = ("89","90","99","98")
-for mark in marks:
-    print(mark)
